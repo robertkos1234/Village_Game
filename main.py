da13c12308d5866d938c0c8e0ab55bb9dc0a9c8d
Роберт <robert.kos1234@gmail.com>
2022-11-01 22:41:58 +0300

    v 0.0.4

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ bot = telebot.TeleBot(token)
 @bot.message_handler(commands=["start"])
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-    button = types.KeyboardButton("Привет")
-    markup.add(button)
+    button1 = types.KeyboardButton("Привет")
+    button2 = types.KeyboardButton("Создать деревню")
+    markup.add(button1, button2)
 
     bot.send_message(message.from_user.id, Text.start, reply_markup=markup)
 
@@ -20,6 +21,11 @@ def start(message):
 def main(message):
     if message.text == "Привет":
         bot.send_message(message.from_user.id, "И тебе привет")
+    elif message.text == "Создать деревню":
+        """ LOG:: Добавить проверку на уже созданную деревню из БД"""
+        bot.send_message(message.from_user.id,Text.new_village)
+    else:
+        bot.send_message(message.from_user.id, "Моя твоя не понимать")
 
 
 bot.polling(non_stop=True)
