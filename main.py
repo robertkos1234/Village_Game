5e3d99871dbed87f038327f6a1334964372ce8bf
Роберт <robert.kos1234@gmail.com>
2022-11-01 12:00:47 +0300

    v 0.0.3

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import telebot
 from telebot import types
 from datetime import datetime
+from text import Text
 
-token = "5109635135:AAFQ1zCIZ8-Xs34LnPLiS5V9yN-VdkbXZ34"
-bot = telebot.TeleBot("5109635135:AAFQ1zCIZ8-Xs34LnPLiS5V9yN-VdkbXZ34")
+token = "5398483352:AAE572M-khWSlhb63v5u8Qcg4hM-XW2FNcw"
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    button = types.KeyboardButton("Привет")
+    markup.add(button)
+
+    bot.send_message(message.from_user.id, Text.start, reply_markup=markup)
+
 
 @bot.message_handler(content_types=["text"])
 def main(message):
     if message.text == "Привет":
-        bot.send_message(message.from_user.id,"И тебе привет")
+        bot.send_message(message.from_user.id, "И тебе привет")
+
 
 bot.polling(non_stop=True)
