d1fb756177c35eb3021aca452301525bd585f7dd
Роберт <robert.kos1234@gmail.com>
2022-11-05 19:13:57 +0300

    v 0.0.7
    + __new__

--- quest.py
+++ quest.py
@@ -1,12 +1,21 @@
 class Quest:
+    hp = 100
+    power = 10
     name = ''
-
 
 
 class Forest(Quest):
     """ Квесты с лесом"""
 
-    pass
+    def __new__(cls, *args, **kwargs):
+        instance = super().__new__(cls)
+        instance.hp = cls.hp - 10
+        instance.power = cls.power + 5
+        return instance
 
 
+rob = Quest()
+elf = Forest()
 
+print(elf.hp)
+print(rob.hp)
